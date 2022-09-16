f1f5a40c57bc378473c49cb9f6a40e694e9487f2
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-09-16 19:20:45 -0300

    test: registering with same password

--- src/test/java/com/yamamoto64/RegisterFeature.java
+++ src/test/java/com/yamamoto64/RegisterFeature.java
@@ -28,6 +28,24 @@ public class RegisterFeature {
 		MobileElement error = (MobileElement) driver.driver.findElementById("br.com.alura.aluraesporte:id/erro_cadastro");
 		
 		Assert.assertEquals("Senhas não conferem", error.getText());
-
+	}
+	
+	@Test
+	public void can_register_with_same_passwords() throws MalformedURLException {
+		
+		AppiumDriverConfig driver =  new AppiumDriverConfig();
+			
+		MobileElement registerButton = (MobileElement) driver.driver.findElementById("br.com.alura.aluraesporte:id/login_botao_cadastrar_usuario");
+		registerButton.click();
+		MobileElement nameForm = (MobileElement) driver.driver.findElementById("br.com.alura.aluraesporte:id/input_nome");
+		MobileElement passForm = (MobileElement) driver.driver.findElementById("br.com.alura.aluraesporte:id/input_senha");
+		MobileElement confirmPassForm = (MobileElement) driver.driver.findElementById("br.com.alura.aluraesporte:id/input_confirmar_senha");
+		nameForm.setValue("Bugan");
+		passForm.setValue("123");
+		confirmPassForm.setValue("123");
+		
+		MobileElement confirmRegisterButton = (MobileElement) driver.driver.findElementById("br.com.alura.aluraesporte:id/cadastro_usuario_botao_cadastrar");
+		confirmRegisterButton.click();
+		
 	}
 }
