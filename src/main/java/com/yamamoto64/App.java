9444740ad36f5bfe23da57e8a0ae1e18287bfc4f
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-09-16 16:44:14 -0300

    starting app on emulator

--- src/main/java/com/yamamoto64/App.java
+++ src/main/java/com/yamamoto64/App.java
@@ -1,5 +1,27 @@
 package com.yamamoto64;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.remote.MobilePlatform;
+
 public class App {
 
+	public static void main(String[] args) throws MalformedURLException {
+		File apk = new File("C:\\projetos\\sport-appium-apk\\src\\main\\resources\\alura_esporte.apk");
+		
+		DesiredCapabilities config = new DesiredCapabilities();
+		config.setCapability(MobileCapabilityType.APP, apk.getAbsolutePath());
+		config.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
+		config.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+		
+		URL urlConnection = new URL("http://127.0.0.1:4723/wd/hub");
+		AppiumDriver driver = new AppiumDriver<>(urlConnection, config);
+	}
+	
 }
--- src/test/java/com/yamamoto64/AppTest.java
+++ src/test/java/com/yamamoto64/AppTest.java
@@ -2,4 +2,5 @@ package com.yamamoto64;
 
 public class AppTest {
 
+	
 }
