ceab8074a94bac6366a6b3556c9afa846b8b77cc
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-09-17 11:50:08 -0300

    Object by login page

--- src/main/java/com/yamamoto64/pageobjects/LoginPO.java
+++ src/main/java/com/yamamoto64/pageobjects/LoginPO.java
@@ -1,20 +1,24 @@
 package com.yamamoto64.pageobjects;
 
+import org.openqa.selenium.By;
+
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 
 public class LoginPO extends BasePO {
 
 	private MobileElement registerButton;
+	
+	private final By registerButtonID;
 
 	public LoginPO(AppiumDriver driver) {
 		super(driver);
+		registerButtonID = By.id("br.com.alura.aluraesporte:id/login_botao_cadastrar_usuario");
 	}
 
 	@Override
 	public void findElements() {
-		registerButton = (MobileElement) driver
-				.findElementById("br.com.alura.aluraesporte:id/login_botao_cadastrar_usuario");
+		registerButton = (MobileElement) driver.findElement(registerButtonID);
 	}
 
 	public RegisterPO goToLoginScren() {
